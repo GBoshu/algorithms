5a8881e36e8b36a622bbf785fd7f6a2b24612202
boshu <guoboshu@hotmail.com>
2016-11-13 15:02:47 +0800

    27_removeElement: accepted, without ut

---
+++ leetcode/src/27_removeElement.cpp
@@ -0,0 +1,150 @@
+/*
+ *      27. Remove Element
+ *
+ *      tag: [array]
+ *      level: [easy]
+ *      file: 27_removeElement.cpp
+ *      complie: g++ -std=c++11
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <tuple>
+#include <cstring>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <climits>
+#include <utility>
+#include <map>
+
+#include <time.h>
+
+using namespace std;
+
+typedef string T_PARAM1;
+typedef int T_PARAM2;
+typedef vector<string> T_OUT;
+
+struct CaseType {
+    T_PARAM1 i1;
+    T_PARAM2 i2;
+    T_OUT o1;
+    clock_t time_span;
+};
+typedef struct CaseType CASETYPE;
+
+class Runable {
+public:
+    virtual string getName() { return "Runable"; }
+    virtual T_OUT run(CASETYPE &c) = 0;
+};
+
+/*************************************************************************/
+
+#ifdef DEBUG
+#define MYDEBUG(x) do { std::cerr << x << endl; } while (0)
+#else
+#define MYDEBUG(x)
+#endif
+
+/*************************************************************************/
+
+class UTbox {
+public:
+    bool runCase(Runable &s, CASETYPE &c) {
+        T_OUT &expect = c.o1;
+        clock_t start, end;
+        start = clock();
+
+        T_OUT out = s.run(c);
+
+        end = clock();
+        c.time_span = end - start;
+
+        return isSame(out, expect);
+    }
+
+    void addSolution(Runable *item) {
+        solutions.push_back(item);
+    }
+
+    void addCase(CASETYPE *cs) {
+        cases.push_back(cs);
+    }
+
+    void runAll() {
+        for (auto s : solutions) {
+            cout << "UT for " << s->getName() << endl;
+            cout << "--------------------------" << endl;
+            for (int i = 0; i != static_cast<int>(cases.size()); ++i) {
+
+                if (runCase(*s, *(cases[i]))) {
+                    cout << "case " << i << " passed: " << (cases[i])->time_span << endl;
+                } else {
+                    cout << "case " << i << " failed" << endl;
+                }
+            }
+            cout << "--------------------------" << endl;
+        }
+    }
+
+
+private:
+    bool isSame(T_OUT &a, T_OUT &b) {
+        return a == b;
+    }
+
+private:
+    vector<Runable *> solutions;
+    vector<CASETYPE *> cases;
+};
+
+/*************************************************************************/
+
+namespace first {
+class Solution : public Runable {
+public:
+    string getName() { return "my first method"; }
+
+    T_OUT run(CASETYPE &c) {
+        return removeElement(c.i1);
+    }
+
+    int removeElement(vector<int>& nums, int val) {
+        int p1 = 0;
+        int p2 = nums.size() - 1;
+
+        while (p1 <= p2) {
+            while (nums[p2] == val && p2 >= p1) --p2;
+            if (p1 > p2) break;
+            if (nums[p1] == val) {
+                nums[p1] = nums[p2--];
+            }
+            p1++;
+        }
+
+        return p2 + 1;
+    }
+};
+}
+
+/*************************************************************************/
+/* testcase
+[3,2,2,3]
+3
+[3,3]
+3
+[3,2,2,3]
+4
+[]
+4
+*/
+/*************************************************************************/
+int main() {
+    UTbox utbox;
+
+    utbox.runAll();
+}
+
